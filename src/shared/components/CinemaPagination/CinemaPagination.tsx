71c5a4d066d07e135654243f28849bc5bba8532a
itolog <paukanlove@gmail.com>
2020-04-23 16:33:54 +0300

    remove arrow pagination

--- src/shared/components/CinemaPagination/CinemaPagination.tsx
+++ src/shared/components/CinemaPagination/CinemaPagination.tsx
@@ -18,35 +18,8 @@ const CinemaPagination: React.FC<Props> = memo(
   ({ children, prev, next, prevLink, nextLink, lastPage, currentPage, showClassicPagination = true }) => {
     return (
       <div className='cinema-pagination'>
-        {/*  Arrow Pagination */}
-        <div className='arrow-pagination'>
-          <div className='cinema-pagination--nav'>
-            {prevLink > 0 && (
-              <button
-                onClick={prev}
-                className='cinema-pagination--btn'
-                title='назад'
-              >
-                &laquo;
-              </button>
-            )}
-          </div>
-
           <div className='cinema-pagination--children'>{children}</div>
 
-          <div className='cinema-pagination--nav'>
-            {nextLink <= lastPage && (
-              <button
-                onClick={next}
-                className='cinema-pagination--btn'
-                title='вперёд'
-              >
-                &raquo;
-              </button>
-            )}
-          </div>
-        </div>
-        {/*  Classic Pagination */}
         {showClassicPagination && <Pagination currentPage={currentPage} lastPage={lastPage}/>}
       </div>
     );
