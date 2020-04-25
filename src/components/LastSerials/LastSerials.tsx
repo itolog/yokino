a2108dda1a44a431da778331119c66650a4706c9
itolog <paukanlove@gmail.com>
2020-04-25 18:27:31 +0300

    refactoring Loader in LastSerials

--- src/components/LastSerials/LastSerials.tsx
+++ src/components/LastSerials/LastSerials.tsx
@@ -1,55 +1,150 @@
+import { makeStyles } from '@material-ui/core/styles';
+import React, { memo, useEffect, useState } from 'react';
+
 import { useQuery } from '@apollo/react-hooks';
 import { Link } from 'gatsby';
-import React, { memo } from 'react';
 
+import Skeleton from '@material-ui/lab/Skeleton';
 import Waiting from '../../assets/img/waiting.svg';
 import { LastUpdateItems } from '../../shared/generated/graphql';
 import { LIST_FOR_SERIALS_UPDATES } from '../../shared/ggl/getLastSerialsUpdates';
-import SpinLoader from '../../shared/UI/SpinLoader/SpinLoader';
-import './lastSerials.scss';
+
+const useStyles = makeStyles((theme) => ({
+  lastSerials: {
+    display: 'flex',
+    flexFlow: 'column',
+    width: '90%',
+    [ theme.breakpoints.down('sm') ]: {
+      flexFlow: 'row',
+      overflowX: 'auto',
+      overflowY: 'hidden',
+      width: '250px',
+      minHeight: '200px',
+      marginBottom: '5%',
+      '&::-webkit-scrollbar': {
+        display: 'none',
+      },
+    },
+  },
+  lastSerialsItems: {
+    position: 'relative',
+    textAlign: 'center',
+    margin: '1px',
+    borderRadius: '5px',
+    boxShadow: '0px 0px 8px 2px rgb(26, 2, 26)',
+    perspective: '1410px',
+    perspectiveOrigin: '45% 50%',
+    transition: ' 0.5s',
+    backgroundImage: `linear-gradient(
+      to right,
+      #1a143b,
+      #24143d,
+      #2e133f,
+      #381240,
+      #421040,
+      #421040,
+      #421040,
+      #421040,
+      #381240,
+      #2e133f,
+      #24143d,
+      #1a143b
+    )`,
+    '&:hover': {
+      boxShadow: '0px 0px 12px 5px rgba(0, 0, 0, 1)',
+      transform: 'scale(1.1) scaleZ(1) rotateZ(0deg)',
+      zIndex: '20',
+    },
+    [ theme.breakpoints.down('sm') ]: {
+      transform: 'scale(1.1) scaleZ(1) rotateZ(0deg)',
+      margin: '0 5%',
+    },
+  },
+  episodesTitle: {
+    fontSize: '1em',
+  },
+  episodesSerial: {
+    position: 'relative',
+    color: 'rgb(206, 232, 241)',
+    fontWeight: 'bold',
+    height: '100%',
+    top: '0',
+    display: 'flex',
+    flexFlow: 'column',
+    width: '100%',
+    justifyContent: 'space-around',
+    zIndex: '2',
+    [ theme.breakpoints.down('sm') ]: {
+      minWidth: '200px',
+      width: 'auto',
+    },
+  },
+
+  episodesSerialContent: {
+    display: 'flex',
+    flexFlow: 'wrap column',
+  },
+  serialsHref: {
+    height: '100%',
+  },
+  waiting: {
+    position: ' absolute',
+    bottom: '0',
+    width: '20px',
+    height: '20px',
+    [ theme.breakpoints.down('sm') ]: {
+      bottom: '10px',
+    },
+  },
+  skelet: {
+    background: '#490e50',
+  },
+}));
+
 
 const LastSerials = memo(() => {
-  const { loading, error, data } = useQuery(LIST_FOR_SERIALS_UPDATES);
-  const serilas: LastUpdateItems[] = data && data.lastUpdate.items;
+  const classes = useStyles();
+  const [ serials, setSerials ] = useState<Array<any>>([ 1, 2, 3, 4, 5, 6 ]);
 
-  /**
-   * Get uniq Serials.Now Api return non uniq episodes.
-   */
-  const serialsSet = serilas && [ ...new Set(serilas.map((item: any) => item)) ];
+  const { loading, error, data } = useQuery(LIST_FOR_SERIALS_UPDATES);
+
+  useEffect(() => {
+    if (data) {
+      const serilas: LastUpdateItems[] = data.lastUpdate.items;
+      /**
+       * Get uniq Serials.Now Api return non uniq episodes.
+       */
+      const serialsSet = serilas && [ ...new Set(serilas.map((item: any) => item)) ];
+      setSerials(serialsSet);
+    }
+  }, [ data ]);
 
   if (error) return <h2>{error.message}</h2>;
 
-  if (loading)
-    return (
-      <div className='update-spiner'>
-        <SpinLoader/>
-      </div>
-    );
-
   return (
-    <ul className='last-serials'>
-      {!loading &&
-      serialsSet.map((item: LastUpdateItems, index: number) => {
+    <ul className={classes.lastSerials}>
+      {serials.map((item: LastUpdateItems, index: number) => {
         return (
-          <li className='last-serials--items' key={index}>
+          <li className={classes.lastSerialsItems} key={index}>
             <Link
               to={`/video/?id=${item.id}`}
               aria-label='navigate to the video page'
-              className='serials-href'
+              className={classes.serialsHref}
             >
-              {/* <LazyImg
-                src={item.material_data.poster_url}
-                alt={item.title}
-                height='140'
-                width='120'
-              /> */}
-              <div className='episodes-serial'>
-                <span className='episodes-title'>{item.name}</span>
-                <div>
-                  <span>{item.season} сезон </span>
-                  <span>{item.episode} серия</span>
+              <div className={classes.episodesSerial}>
+                {!loading ?
+                  (<span className={classes.episodesTitle}>{item.name}</span>)
+                  : (<Skeleton className={classes.skelet} variant='text'/>)
+                }
+                <div className={classes.episodesSerialContent}>
+                  {!loading ?
+                    (<span>{item.season} сезон </span>)
+                    : (<Skeleton className={classes.skelet} variant='text'/>)}
+                  {!loading ?
+                    (<span>{item.episode} серия</span>)
+                    : (<Skeleton className={classes.skelet} variant='text'/>)}
                 </div>
-                {!item.iframe_url && <div className='waiting' aria-label='waiting' title='ожидается'>
+                {!item.iframe_url && <div className={classes.waiting} aria-label='waiting' title='ожидается'>
                   <Waiting/>
                 </div>}
               </div>
